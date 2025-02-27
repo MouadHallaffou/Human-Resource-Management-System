ec0d84625eb35d47677ec451a07fcf01ae1a1a89
MouadHallaffou <mouadhallaffou@gmail.com>
2025-02-27 22:41:10 +0000

    HRMS-2 feat: Add Department entity with model, migration, and controller

---
+++ app/Models/Departement.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Departement extends Model
+{
+    /** @use HasFactory<\Database\Factories\DepartementFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'company_id',
+        // 'responsable_id',
+    ];
+
+    /**
+     * Relation avec le modèle User (responsable du département).
+     */
+    public function responsable()
+    {
+        return $this->belongsTo(User::class, 'responsable_id');
+    }
+
+    /**
+     * Relation avec le modèle Company.
+     */
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    /**
+     * Relation avec le modèle User (employés du département).
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+    
+}
