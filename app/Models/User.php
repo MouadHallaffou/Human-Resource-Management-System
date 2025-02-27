dc83b5b08f2af9dd813d71e492c9347b7853a166
MouadHallaffou <mouadhallaffou@gmail.com>
2025-02-27 22:39:59 +0000

    HRMS-1 feat: Add User entity with model, migration, and controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Departement;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -28,10 +30,11 @@ class User extends Authenticatable
         'recruitment_date', 
         'salary',          
         'status',          
-        // 'role_id',         
-        // 'department_id',   
-        // 'company_id',      
+        'role_id',         
+        'department_id',   
+        'contract_id', 
     ];
+    
 
     /**
      * The attributes that should be hidden for serialization.
@@ -55,4 +58,19 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function departement(){
+        return $this->belongsToMany(Departement::class);
+    }
+
+    public function formation()
+    {
+        return $this->belongsTo(Formation::class);
+    }
+
+    public function contract()
+    {
+        return $this->belongsTo(Contract::class);
+    }
+
 }
