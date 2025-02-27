d6c6ef51c99c79b858759e176dfec84cb776bab1
MouadHallaffou <mouadhallaffou@gmail.com>
2025-02-27 22:41:58 +0000

    HRMS-4 feat: Add Formation entity with model, migration, and controller

---
+++ app/Models/Formation.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Formation extends Model
+{
+    /** @use HasFactory<\Database\Factories\FormationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'location',
+        'certificate',
+        'start_date',
+        'end_date',
+    ];
+
+    // Accesseur pour start_date
+    public function getStartDateAttribute($value)
+    {
+        return Carbon::parse($value)->format('d/m/Y');
+    }
+
+    // Accesseur pour end_date
+    public function getEndDateAttribute($value)
+    {
+        return Carbon::parse($value)->format('d/m/Y');
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+    
+}
