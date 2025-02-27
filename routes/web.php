d3aa51549a053f9017040eeac205b3722741cf84
MouadHallaffou <mouadhallaffou@gmail.com>
2025-02-27 22:57:36 +0000

    HMRS-2 Add routes for all entities

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Models\Departement;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\JoobsController;
+use App\Http\Controllers\ContractController;
+use App\Http\Controllers\FormationController;
+use App\Http\Controllers\DepartementController;
 
 Route::view('/', 'welcome');
 
@@ -11,5 +17,12 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+    
+Route::resource('departements', DepartementController::class);
+Route::resource('contracts', ContractController::class);
+Route::resource('joobs', JoobsController::class);
+Route::resource('formations', FormationController::class);
+Route::resource('users', UserController::class);
+
 
 require __DIR__.'/auth.php';
