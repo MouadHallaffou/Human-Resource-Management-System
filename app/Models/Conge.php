66aa039a59ddb7fcd34cbd4709a5423626b60e3d
MouadHallaffou <mouadhallaffou@gmail.com>
2025-03-06 11:32:36 +0000

    HRMS-50: Ajout du modèle /app/Models/Conge.php pour gérer les demandes de congé

---
+++ app/Models/Conge.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Conge extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'start_date',
+        'end_date',
+        'total_days',
+        'status_manager',
+        'status_rh_manager',
+        'status_demandeur',
+        'cause',
+    ];
+
+    // Relation avec l'utilisateur (employé)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
